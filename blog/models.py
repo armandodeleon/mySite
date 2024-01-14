e90f5a4bae704aea7bf5607609b4fc768392eb96
Armando De Leon Del Bosque <armandodeleon@outlook.com>
2024-01-13 21:41:26 -0600

    Canonical URLs

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -10,7 +11,6 @@ class PublishedManager(models.Manager):
 
 # Create your models here.
 class Post(models.Model):
-
     DRAFT = 'Draft'
     PUBLISHED = 'Published'
     STATUS_CHOICES = [
@@ -19,7 +19,7 @@ class Post(models.Model):
     ]
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')  # unique for the date stored
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -42,3 +42,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])  # blog namespace (mySite/urls.py):url name (blog/urls.py)
--- blog/views.py
+++ blog/views.py
@@ -13,10 +13,13 @@ def post_list(request):
                   {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.PUBLISHED)
+                             status=Post.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
